8a1f00487bdc3eaaedb4cc3ccd9c3bbe7c85b844
koytipita <mrtdmrtbn@gmail.com>
2024-05-18 18:04:41 +0300

    day3 - part1

---
+++ day3/Map.java
@@ -0,0 +1,40 @@
+package day3;
+
+import day3.MapObject;
+import java.util.List;
+
+public class Map {
+    private Integer horizontalSize;
+    private Integer verticalSize;
+    private List<MapObject> mapObjects;
+
+    public Map(Integer horizontalSize, Integer verticalSize, List<MapObject> mapObjects) {
+        this.horizontalSize = horizontalSize;
+        this.verticalSize = verticalSize;
+        this.mapObjects = mapObjects;
+    }
+
+    public Integer getHorizontalSize() {
+        return horizontalSize;
+    }
+
+    public void setHorizontalSize(Integer horizontalSize) {
+        this.horizontalSize = horizontalSize;
+    }
+
+    public Integer getVerticalSize() {
+        return verticalSize;
+    }
+
+    public void setVerticalSize(Integer verticalSize) {
+        this.verticalSize = verticalSize;
+    }
+
+    public List<MapObject> getMapObjects() {
+        return mapObjects;
+    }
+
+    public void setMapObjects(List<MapObject> mapObjects) {
+        this.mapObjects = mapObjects;
+    }
+}
---
+++ day3/day3.java
@@ -0,0 +1,68 @@
+package day3;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class day3 {
+    static Logger logger = Logger.getLogger(day3.class.getName());
+    static final String EXAMPLE_FILE_PATH = "day3/example.txt";
+    static final String ACTUAL_FILE_PATH = "day3/input.txt";
+    static int sum = 0;
+
+    public static List<Integer> getNeighborNumbers(MapObject sign, Map map){
+        Integer x = sign.getLocationStartIndexX();
+        Integer y = sign.getLocationStartIndexY();
+        return map.getMapObjects().stream()
+                .filter(a -> a.getObjType().equals(MapObject.ObjTypeEnum.NUMBER))
+                .filter(number -> {
+                    boolean yRange = Math.max(y - 1, number.getLocationStartIndexY()) == Math.min(number.getLocationStartIndexY(), y + 1);
+                    boolean xRange = Math.max(x - number.getLength(), number.getLocationStartIndexX()) == Math.min(number.getLocationStartIndexX(), x + 1);
+                    return yRange && xRange;
+                }).mapToInt(MapObject::getValue).boxed().collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args) {
+        List<String> lines = utils.FileParseUtil.readLinesFromFile(ACTUAL_FILE_PATH, logger);
+        Map map = new Map(lines.getFirst().length(),lines.size(),new ArrayList<>());
+        List<Integer> numbersToSum = new ArrayList<>();
+
+        for (int i = 0; i < lines.size(); i++) {
+            Pattern patternNumber = Pattern.compile("\\d+");
+
+            Matcher matcherNumber = patternNumber.matcher(lines.get(i));
+
+            Pattern patternSign = Pattern.compile("[^\\d.]");
+
+            Matcher matcherSign = patternSign.matcher(lines.get(i));
+
+            // Loop through all matches
+            while (matcherSign.find()) {
+                int index = matcherSign.start();
+                map.getMapObjects().add(new MapObject(MapObject.ObjTypeEnum.SIGN,
+                        index,i,1,-1));
+            }
+
+            // Loop through all matches
+            while (matcherNumber.find()) {
+                String number = matcherNumber.group();
+                int index = matcherNumber.start();
+                map.getMapObjects().add(new MapObject(MapObject.ObjTypeEnum.NUMBER,
+                        index,i,number.length(),Integer.parseInt(number)));
+            }
+        }
+        for (MapObject mapObject : map.getMapObjects()){
+            if (mapObject.getObjType().equals(MapObject.ObjTypeEnum.SIGN)){
+                numbersToSum.addAll(getNeighborNumbers(mapObject,map));
+            }
+        }
+        System.out.println(numbersToSum.stream().reduce(0, Integer::sum));
+
+    }
+}
