41f5c0b5ee3286cef4560ec05f063276c5aaa73b
koytipita <mrtdmrtbn@gmail.com>
2024-05-26 14:55:02 +0300

    day15 - part2

--- day15/day15.java
+++ day15/day15.java
@@ -1,7 +1,7 @@
 package day15;
 
 
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
@@ -12,6 +12,7 @@ public class day15 {
     static final String EXAMPLE2_FILE_PATH = "day15/example2.txt";
     static final String EXAMPLE3_FILE_PATH = "day15/example3.txt";
     static final String ACTUAL_FILE_PATH = "day15/input.txt";
+    static Map<Integer,List<Lens>> boxes = new HashMap<>();
 
     public static Integer calculateHash(String word){
         int currentValue = 0;
@@ -24,10 +25,53 @@ public class day15 {
         return currentValue;
     }
 
+    public static void executeCommands(List<String> commands){
+
+        commands.forEach(command -> {
+            int dashSignIndex = command.indexOf("-");
+            int equalsSignIndex = command.indexOf("=");
+            int signIndex = dashSignIndex == -1 ? equalsSignIndex : dashSignIndex;
+            String word = command.substring(0,signIndex);
+            int hashValue = calculateHash(word);
+            List<Lens> lensList= boxes.get(hashValue);
+            if(signIndex == dashSignIndex){
+                Optional<Integer> indexToBeRemoved = IntStream.range(0, lensList.size())
+                        .filter(index -> lensList.get(index).getWord().equals(word)).boxed().findFirst();
+                indexToBeRemoved.ifPresent(index -> lensList.remove(index.intValue()));
+            }
+            if(signIndex == equalsSignIndex){
+                Integer focalLength = Integer.valueOf(command.substring(equalsSignIndex+1));
+                Optional<Integer> indexToBeReplaced = IntStream.range(0, lensList.size())
+                        .filter(index -> lensList.get(index).getWord().equals(word)).boxed().findFirst();
+                indexToBeReplaced.ifPresentOrElse(
+                        index -> lensList.set(index,new Lens(word,focalLength)),
+                        () -> lensList.add(new Lens(word,focalLength))
+                );
+            }
+        });
+    }
+
+    public static Integer calculateSumOfBoxes(){
+        Integer sum = 0;
+        for (int key = 0; key < 256; key++) {
+            List<Lens> lensList = boxes.get(key);
+            for (int indexInList = 0; indexInList < lensList.size(); indexInList++) {
+                sum += (key+1) * (indexInList+1) * lensList.get(indexInList).getFocalLength();
+            }
+        }
+        return sum;
+    }
+
 
     public static void main(String[] args) {
         List<String> words = utils.FileParseUtil.readStringsFromFile(ACTUAL_FILE_PATH, logger);
-        Integer sum = words.stream().mapToInt(day15::calculateHash).sum();
-        logger.log(Level.INFO, String.valueOf(sum));
+        for (int i = 0; i < 256; i++) {
+            boxes.put(i,new ArrayList<>());
+        }
+        //Integer sum1 = words.stream().mapToInt(day15::calculateHash).sum();
+        executeCommands(words);
+        Integer sum2 = calculateSumOfBoxes();
+        //logger.log(Level.INFO, String.valueOf(sum1));
+        logger.log(Level.INFO, String.valueOf(sum2));
     }
 }
