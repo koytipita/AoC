356f760e547ea6001851fadb25cc8ff122f0f757
koytipita <mrtdmrtbn@gmail.com>
2024-06-08 00:28:55 +0300

    day20 part1 trial

---
+++ day20/trial2/FlipFlop.java
@@ -0,0 +1,72 @@
+package day20.trial2;
+
+import java.util.List;
+
+public class FlipFlop implements Item{
+
+    private String name;
+    private String status;
+    private List<String> connectedInputNames;
+    private List<String> connectedOutputNames;
+
+    public FlipFlop(String name, String status, List<String> connectedInputNames, List<String> connectedOutputNames) {
+        this.name = name;
+        this.status = status;
+        this.connectedInputNames = connectedInputNames;
+        this.connectedOutputNames = connectedOutputNames;
+    }
+
+    public void pulseReceive(String pulse, String inputName){
+        if (pulse.equals("LOW")){
+            if (status.equals("OFF")){
+                this.status = "ON";
+            }
+            else{
+                this.status = "OFF";
+            }
+        }
+    }
+
+    public List<Pulse> pulseTransfer(String pulse){
+        if (pulse.equals("LOW")){
+            return connectedOutputNames.stream()
+                .map(connectedOutputName ->
+                    status.equals("ON") ? new Pulse(this.name, connectedOutputName,"HIGH") :
+                        new Pulse(this.name, connectedOutputName,"LOW")
+                )
+                .toList();
+        }
+        return null;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<String> getConnectedInputNames() {
+        return connectedInputNames;
+    }
+
+    public void setConnectedInputNames(List<String> connectedInputNames) {
+        this.connectedInputNames = connectedInputNames;
+    }
+
+    public List<String> getConnectedOutputNames() {
+        return connectedOutputNames;
+    }
+
+    public void setConnectedOutputNames(List<String> connectedOutputNames) {
+        this.connectedOutputNames = connectedOutputNames;
+    }
+}
---
+++ day20/trial2/Pulse.java
@@ -0,0 +1,37 @@
+package day20.trial2;
+
+public class Pulse {
+    private String from;
+    private String to;
+    private String value;
+
+    public Pulse(String from, String to, String value) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
