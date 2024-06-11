4bf6a88ef3e44aeb4cdff17623ab4e4d26727775
koytipita <mrtdmrtbn@gmail.com>
2024-06-11 22:23:06 +0300

    day21 part1

--- day21/day21.java
+++ day21/day21.java
@@ -16,10 +16,10 @@ public class day21 {
     private static final int OFFSET = 3;
 
     public static void main(String[] args) {
-        map = utils.FileParseUtil.readLinesFromFile(EXAMPLE1_FILE_PATH, logger);
+        map = utils.FileParseUtil.readLinesFromFile(ACTUAL_FILE_PATH, logger);
         sizeX = map.getFirst().length();
         sizeY = map.size();
-        int stepLeft = 6;
+        int stepLeft = 64;
         Position startPosition = getStartPosition(stepLeft);
         greedyPath(startPosition.getX(), startPosition.getY(), stepLeft);
         System.out.println(visitedPlots.size());
@@ -33,102 +33,46 @@ public class day21 {
         return shiftedX * 10 + shiftedY; // Combine the values uniquely
     }
 
-    // Decode the int value back into two integers
-    public static int[] decode(int encoded) {
-        int shiftedX = encoded / 10;
-        int shiftedY = encoded % 10;
-        int x = shiftedX - OFFSET;
-        int y = shiftedY - OFFSET;
-        return new int[]{x, y};
-    }
-
-    public static boolean checkPathValid(int x, int y, Position move, Integer stepLeft){
+    public static boolean checkPathValid(int x, int y, Position move){
         int moveX = move.getX();
         int moveY = move.getY();
         List<Position> stations = new ArrayList<>();
         int encoded = encode(moveX, moveY);
-
         switch (encoded) {
-            case 13: // (-2, 0)
-                boolean insideBoundaries13 = x+moveX >= 0 && y < sizeY && y >= 0;
-                if(insideBoundaries13 && (map.get(y).substring(x+moveX,x).contains("#"))){
+            case 23: // (-1, 0)
+                boolean insideBoundaries23 =  x+moveX >= 0 && y+moveY < sizeY && y+moveY >= 0 && x+moveX < sizeX;
+                if( insideBoundaries23 && (map.get(y).substring(x-1,x).contains("#"))){
                     break;
                 }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
+                stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
-            case 24: // (-1, 1)
-                boolean insideBoundaries24 =  x+moveX >= 0 && y+moveY < sizeY && y+moveY >= 0 && x+moveX < sizeX;
-                if( insideBoundaries24 && !((!map.get(y).substring(x+moveX,x).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x).contains("#"))
-                    || (!map.get(y+moveY).substring(x,x+1).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x).contains("#")))){
+            case 32: // (0, -1)
+                boolean insideBoundaries32 = x+moveX >= 0 && y+moveY >= 0 && x+moveX < sizeX && y+moveY <sizeY;
+                if( insideBoundaries32 &&  map.get(y-1).substring(x,x+1).contains("#")){
                     break;
                 }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
+                stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
-            case 22: // (-1, -1)
-                boolean insideBoundaries22 = x+moveX >= 0 && y+moveY >= 0 && x+moveX < sizeX && y+moveY <sizeY;
-                if( insideBoundaries22
-                    && !((!map.get(y).substring(x+moveX,x).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x).contains("#"))
-                    || (!map.get(y+moveY).substring(x,x+1).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x).contains("#")))){
+            case 34: // (0, 1)
+                boolean insideBoundaries34 = x+moveX < sizeX && y+moveY >= 0 && x+moveX >=0 && y+moveY <sizeY;
+                if(insideBoundaries34 && map.get(y+1).substring(x,x+1).contains("#")){
                     break;
                 }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
+                stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
-            case 35: // (0, 2)
-                boolean insideBoundaries35 = y+moveY < sizeY  && x < sizeX && x >= 0;
-                if(insideBoundaries35 && !(!map.get(y+moveY-1).substring(x,x+1).contains("#")
-                    && !map.get(y+moveY).substring(x,x+1).contains("#"))){
-                    break;
-                }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
-                break;
-            case 31: // (0, -2)
-                boolean insideBoundaries31 = y+moveY >= 0 && x < sizeX && x >= 0;
-                if(insideBoundaries31
-                    && !(!map.get(y+moveY+1).substring(x,x+1).contains("#")
-                    && !map.get(y+moveY).substring(x,x+1).contains("#"))){
-                    break;
-                }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
-                break;
-            case 42: // (1, -1)
-                boolean insideBoundaries42 = x+moveX < sizeX && y+moveY >= 0 && x+moveX >=0 && y+moveY <sizeY;
-                if(insideBoundaries42
-                    && !((!map.get(y).substring(x+moveX,x+moveX+1).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x+moveX+1).contains("#"))
-                    || (!map.get(y+moveY).substring(x,x+1).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x+moveX+1).contains("#")))){
-                    break;
-                }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
-                break;
-            case 44: // (1, 1)
+            case 43: // (1, 0)
                 boolean insideBoundaries44 = x+moveX < sizeX && y+moveY < sizeY && y+moveY >= 0 && x+moveX >=0;
-                if( insideBoundaries44
-                    && !((!map.get(y).substring(x+moveX,x+moveX+1).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x+moveX+1).contains("#"))
-                    || (!map.get(y+moveY).substring(x,x+1).contains("#")
-                    && !map.get(y+moveY).substring(x+moveX,x+moveX+1).contains("#")))){
+                if( insideBoundaries44 && map.get(y).substring(x+1,x+2).contains("#")){
                     break;
                 }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
-                break;
-            case 53: // (2, 0)
-                boolean insideBoundaries53 = x+moveX < sizeX && y < sizeY && y >= 0 && x>=0;
-                if(insideBoundaries53 && map.get(y).substring(x + 1, x + moveX + 1).contains("#")){
-                    break;
-                }
-                stations.add(new Position(x+moveX,y+moveY, stepLeft));
+                stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
             default:
                 throw new IllegalStateException();
         }
-
-
-        visitedPlots.addAll(stations);
+        if (move.getStepLeft() -1 %2 == 0) {
+            visitedPlots.addAll(stations);
+        }
         return !stations.isEmpty();
     }
 
@@ -149,19 +93,21 @@ public class day21 {
     }
 
     public static void greedyPath(int x, int y, int stepLeft){
-        visitedPlots.add(new Position(x,y,stepLeft));
         if (stepLeft==0){
             return;
         }
-        if (!visitedPlotsContains(new Position(x-2,y, stepLeft-2)) && checkPathValid(x, y, new Position(-2,0, stepLeft-2), stepLeft-2)) greedyPath(x-2,y,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x-1,y-1, stepLeft-2)) && checkPathValid(x, y, new Position(-1,-1, stepLeft-2), stepLeft-2)) greedyPath(x-1,y-1,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x-1,y+1, stepLeft-2)) && checkPathValid(x, y, new Position(-1,1, stepLeft-2), stepLeft-2)) greedyPath(x-1,y+1,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x,y-2, stepLeft-2)) && checkPathValid(x, y, new Position(0,-2, stepLeft-2), stepLeft-2)) greedyPath(x,y-2,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x,y+2, stepLeft-2)) && checkPathValid(x, y, new Position(0,2, stepLeft-2), stepLeft-2)) greedyPath(x,y+2,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x+1,y-1, stepLeft-2)) && checkPathValid(x, y, new Position(1,-1, stepLeft-2), stepLeft-2)) greedyPath(x+1,y-1,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x+1,y+1, stepLeft-2)) && checkPathValid(x, y, new Position(1,1, stepLeft-2), stepLeft-2)) greedyPath(x+1,y+1,stepLeft-2);
-        if (!visitedPlotsContains(new Position(x+2,y, stepLeft-2)) && checkPathValid(x, y, new Position(2,0, stepLeft-2), stepLeft-2)) greedyPath(x+2,y,stepLeft-2);
-
+        if(!visitedPlotsContains(new Position(x-1,y, stepLeft-1))
+            && checkPathValid(x, y, new Position(-1,0, stepLeft)))
+            greedyPath(x-1,y,stepLeft-1);
+        if(!visitedPlotsContains(new Position(x,y-1, stepLeft-1))
+            && checkPathValid(x, y, new Position(0,-1, stepLeft)))
+            greedyPath(x,y-1,stepLeft-1);
+        if(!visitedPlotsContains(new Position(x,y+1, stepLeft-1))
+            && checkPathValid(x, y, new Position(0,1, stepLeft)))
+            greedyPath(x,y+1,stepLeft-1);
+        if(!visitedPlotsContains(new Position(x+1,y, stepLeft-1))
+            && checkPathValid(x, y, new Position(1,0, stepLeft)))
+            greedyPath(x+1,y,stepLeft-1);
     }
 
     public static Position getStartPosition(Integer stepLeft){
