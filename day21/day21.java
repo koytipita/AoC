57a8748a880ebf9deac479383de9133bd36c2acd
koytipita <mrtdmrtbn@gmail.com>
2024-06-12 19:11:31 +0300

    day21 part2

--- day21/day21.java
+++ day21/day21.java
@@ -3,6 +3,9 @@ package day21;
 import java.util.*;
 import java.util.logging.Logger;
 
+import static java.lang.Math.floor;
+import static java.lang.Math.floorMod;
+
 public class day21 {
     static Logger logger = Logger.getLogger(day21.class.getName());
     static final String EXAMPLE1_FILE_PATH = "day21/example1.txt";
@@ -10,7 +13,7 @@ public class day21 {
     static final String EXAMPLE3_FILE_PATH = "day21/example3.txt";
     static final String ACTUAL_FILE_PATH = "day21/input.txt";
     static List<String> map;
-    static Set<Position> visitedPlots = new HashSet<>();
+    static Map<Position, Position> visitedPlots = new HashMap<>();
     static Integer sizeX;
     static Integer sizeY;
     private static final int OFFSET = 3;
@@ -19,12 +22,23 @@ public class day21 {
         map = utils.FileParseUtil.readLinesFromFile(ACTUAL_FILE_PATH, logger);
         sizeX = map.getFirst().length();
         sizeY = map.size();
-        int stepLeft = 64;
-        Position startPosition = getStartPosition(stepLeft);
-        greedyPath(startPosition.getX(), startPosition.getY(), stepLeft);
-        System.out.println(visitedPlots.size());
+        List<Integer> xs = new ArrayList<>();
+        int []stepLefts = new int[]{65,65+131,65+131*2}; //65,65+131,65+131*2 , 7,25,59
+        for (int stepLeft : stepLefts){
+            Position startPosition = getStartPosition(stepLeft);
+            greedyPath(startPosition.getX(), startPosition.getY(), stepLeft);
+            System.out.println(stepLeft + " " + visitedPlots.size());
+            xs.add(visitedPlots.size());
+            visitedPlots.clear();
+        }
+        System.out.println(quad(xs, (long) floor((26501365 -65) / 131.0)));
+    }
 
-
+    public static long quad(List<Integer> numbers , long n){
+        long a = (long) floor((numbers.get(2) - (2L * numbers.get(1)) + numbers.get(0)) / 2.0);
+        long b = numbers.get(1) - numbers.get(0) - a;
+        long c = numbers.get(0);
+        return (a * n * n) + (b * n) + c;
     }
 
     public static int encode(int x, int y) {
@@ -40,25 +54,25 @@ public class day21 {
         int encoded = encode(moveX, moveY);
         switch (encoded) {
             case 23: // (-1, 0)
-                if((map.get(y).substring(x-1,x).contains("#"))){
+                if( map.get(floorMod(y,sizeY)).charAt(floorMod(x-1,sizeX)) == '#'){
                     break;
                 }
                 stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
             case 32: // (0, -1)
-                if( map.get(y-1).substring(x,x+1).contains("#")){
+                if(map.get(floorMod(y-1,sizeY)).charAt(floorMod(x,sizeX)) == '#'){
                     break;
                 }
                 stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
             case 34: // (0, 1)
-                if(map.get(y+1).substring(x,x+1).contains("#")){
+                if(map.get(floorMod(y+1,sizeY)).charAt(floorMod(x,sizeX)) == '#'){
                     break;
                 }
                 stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
                 break;
             case 43: // (1, 0)
-                if(map.get(y).substring(x+1,x+2).contains("#")){
+                if(map.get(floorMod(y,sizeY)).charAt(floorMod(x+1,sizeX)) == '#'){
                     break;
                 }
                 stations.add(new Position(x+moveX,y+moveY, move.getStepLeft()-1));
@@ -66,24 +80,22 @@ public class day21 {
             default:
                 throw new IllegalStateException();
         }
-        if (move.getStepLeft() -1 %2 == 0) {
-            visitedPlots.addAll(stations);
+        if ( (move.getStepLeft() -1) %2 == 0) {
+            for (Position station : stations) {
+                visitedPlots.put(station, station);
+            }
         }
         return !stations.isEmpty();
     }
 
     public static boolean visitedPlotsContains(Position position){
-        if (visitedPlots.contains(position)){
-            for (Position position1 : visitedPlots) {
-                if (position1.equals(position)) {
-                    if (position1.getStepLeft() < position.getStepLeft()) {
-                        visitedPlots.remove(position1);
-                        visitedPlots.add(position);
-                        return false;
-                    }
-                    return true;
-                }
+        if (visitedPlots.containsKey(position)) {
+            Position existingPosition = visitedPlots.get(position);
+            if (existingPosition.getStepLeft() < position.getStepLeft()) {
+                visitedPlots.put(position, position);
+                return false;
             }
+            return true;
         }
         return false;
     }
