ecc249fa38c97026ad1522d0cad28b5615a867fa
koytipita <mrtdmrtbn@gmail.com>
2024-06-13 23:18:03 +0300

    day22 part2

--- day22/Brick.java
+++ day22/Brick.java
@@ -1,5 +1,6 @@
 package day22;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Brick {
@@ -9,6 +10,10 @@ public class Brick {
     public Brick(int[] firstPoint, int[] secondPoint) {
         this.firstPoint = firstPoint;
         this.secondPoint = secondPoint;
+    }
+
+    public Brick copy() {
+        return new Brick(Arrays.copyOf(firstPoint, firstPoint.length), Arrays.copyOf(secondPoint, secondPoint.length));
     }
 
     public int[] getFirstPoint() {
@@ -25,5 +30,20 @@ public class Brick {
 
     public void setSecondPoint(int[] secondPoint) {
         this.secondPoint = secondPoint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brick brick = (Brick) o;
+        return Arrays.equals(firstPoint, brick.firstPoint) && Arrays.equals(secondPoint, brick.secondPoint);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(firstPoint);
+        result = 31 * result + Arrays.hashCode(secondPoint);
+        return result;
     }
 }
--- day22/day22.java
+++ day22/day22.java
@@ -1,6 +1,8 @@
 package day22;
 
 
+import utils.StreamUtil;
+
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -15,14 +17,64 @@ public class day22 {
     static List<Brick> bricks = new ArrayList<>();
     static List<Brick> bricksFallen = new ArrayList<>();
     static Set<Brick> bricksSupporting = new HashSet<>();
+    static long fallingBrickCount = 0L;
 
     public static void main(String[] args) {
         lines = utils.FileParseUtil.readLinesFromFile(ACTUAL_FILE_PATH,logger);
         parseBricks();
         bricks.forEach(day22::brickFall);
         System.out.println(bricks.size()-bricksSupporting.size());
+        Set<Brick> copy = new HashSet<>(bricksSupporting);
+        for (Brick brick: copy ) {
+            countFallingBricks(brick);
+        }
+        System.out.println(fallingBrickCount);
 
     }
+
+    public static void countFallingBricks(Brick brick){
+        List<Brick> alternativeBricks = new ArrayList<>();
+        for (Brick alternativeBrick : bricksFallen) {
+            alternativeBricks.add(alternativeBrick.copy());
+        }
+        alternativeBricks.remove(brick);
+        alternativeBricks.forEach(alternativeBrick -> brickFallSimulation(alternativeBrick,  new ArrayList<>(alternativeBricks)));
+    }
+
+    public static void brickFallSimulation(Brick brick, List<Brick> alternativeBricks){
+        if (canFallSimulation(brick, alternativeBricks)) fallingBrickCount +=1;
+        while (canFallSimulation(brick, alternativeBricks)) {
+            int[] firstPoint = brick.getFirstPoint();
+            int[] secondPoint = brick.getSecondPoint();
+            firstPoint[2] = firstPoint[2] - 1;
+            secondPoint[2] = secondPoint[2] - 1;
+            brick.setFirstPoint(firstPoint);
+            brick.setSecondPoint(secondPoint);
+        }
+        alternativeBricks.add(brick.copy());
+    }
+
+    public static boolean canFallSimulation(Brick brick1,List<Brick> alternativeBricks){
+        if (brick1.getFirstPoint()[2] == 1) return false;
+        return alternativeBricks.stream()
+            .filter(brick2 -> brick2.getSecondPoint()[2] == brick1.getFirstPoint()[2]-1)
+            .noneMatch(brick2 ->{
+                int brick2FirstX = brick2.getFirstPoint()[0];
+                int brick2SecondX = brick2.getSecondPoint()[0];
+                int brick2FirstY = brick2.getFirstPoint()[1];
+                int brick2SecondY = brick2.getSecondPoint()[1];
+                int brick1FirstX = brick1.getFirstPoint()[0];
+                int brick1SecondX = brick1.getSecondPoint()[0];
+                int brick1FirstY = brick1.getFirstPoint()[1];
+                int brick1SecondY = brick1.getSecondPoint()[1];
+                boolean horizontalOverlap = brick1FirstX <= brick2SecondX && brick1SecondX >= brick2FirstX;
+                boolean verticalOverlap = brick1FirstY <= brick2SecondY && brick1SecondY >= brick2FirstY;
+                return horizontalOverlap && verticalOverlap;
+            });
+
+    }
+
+
 
     public static boolean canFall(Brick brick1){
         if (brick1.getFirstPoint()[2] == 1) return false;
@@ -48,7 +100,6 @@ public class day22 {
                 }
                 return list;
             }));
-
 
         return bricksFallen.stream()
             .filter(brick2 -> brick2.getSecondPoint()[2] == brick1.getFirstPoint()[2]-1)
