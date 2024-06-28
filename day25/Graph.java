2f6aa2e9d4e71f70acfb9c15a4cf44583d4dac43
koytipita <mrtdmrtbn@gmail.com>
2024-06-28 17:53:07 +0300

    day25 part1 first trial

---
+++ day25/Graph.java
@@ -0,0 +1,68 @@
+package day25;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Graph {
+    private List<Edge> edgeList = new ArrayList<>();
+    private Integer verticeCount;
+
+    public Graph(List<Edge> edgeList, Integer verticeCount) {
+        this.edgeList = edgeList;
+        this.verticeCount = verticeCount;
+    }
+
+    public void cutEdge(Edge edge){
+        Node node1 = edge.getNode1();
+        Node node2 = edge.getNode2();
+        node1.setCount(node1.getCount()+1);
+
+        verticeCount -= 1;
+        edgeList.remove(edge);
+        edgeList.stream().filter(edge1 -> edge1.getNode1().getName().equals(node2.getName()))
+            .forEach(edge1-> {
+                int index = edgeList.indexOf(edge1);
+                edge1.setNode1(node1);
+                edgeList.set(index, edge1);
+            });
+        edgeList.stream().filter(edge1 -> edge1.getNode2().getName().equals(node2.getName()))
+            .forEach(edge1->{
+                int index = edgeList.indexOf(edge1);
+                edge1.setNode2(node1);
+                edgeList.set(index, edge1);
+            });
+        List<Edge> selfLoops = edgeList.stream().filter(edge1 -> edge1.getNode1().getName().equals(edge1.getNode2().getName())).toList();
+        selfLoops.forEach(selfLoop -> edgeList.remove(selfLoop));
+        node1.setName(node1.getName()+ " " + node2.getName());
+    }
+
+    public boolean addEdge(Edge edge){
+        String str1 = edge.getNode1().getName();
+        String str2 = edge.getNode2().getName();
+        if(edgeList.stream().anyMatch(edge1 ->
+            (edge1.getNode1().getName().equals(str1) && edge1.getNode2().getName().equals(str2)
+            || edge1.getNode1().getName().equals(str2) && edge1.getNode2().getName().equals(str1)))){
+            return false;
+        }
+        edgeList.add(edge);
+        return true;
+    }
+
+    public List<Edge> getEdgeList() {
+        return edgeList;
+    }
+
+    public void setEdgeList(List<Edge> edgeList) {
+        this.edgeList = edgeList;
+    }
+
+    public Integer getVerticeCount() {
+        return verticeCount;
+    }
+
+    public void setVerticeCount(Integer verticeCount) {
+        this.verticeCount = verticeCount;
+    }
+}
---
+++ day25/day25.java
@@ -0,0 +1,59 @@
+package day25;
+
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class day25 {
+    static Logger logger = Logger.getLogger(day25.class.getName());
+    static final String EXAMPLE1_FILE_PATH = "day25/example1.txt";
+    static final String EXAMPLE2_FILE_PATH = "day25/example2.txt";
+    static final String EXAMPLE3_FILE_PATH = "day25/example3.txt";
+    static final String ACTUAL_FILE_PATH = "day25/input.txt";
+    static List<String> lines;
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        lines = utils.FileParseUtil.readLinesFromFile(EXAMPLE1_FILE_PATH, logger);
+        createGraph();
+        List<Edge> cutEdges = kargerMinCut(createGraph());
+        while (cutEdges == null){
+            cutEdges = kargerMinCut(createGraph());
+        }
+        System.out.println(cutEdges.getFirst().getNode1().getName().split(" ").length
+        * cutEdges.getFirst().getNode2().getName().split(" ").length);
+    }
+    public static Graph createGraph(){
+        Graph mainGraph = new Graph(new ArrayList<>(), 0);
+        lines.forEach(line -> {
+            Node node1 = new Node(1,line.split(": ")[0]);
+            List<Node> nodeList = Arrays.stream(line.split(": ")[1]
+                .split(" "))
+                .map(str -> new Node(1,str))
+                .toList();
+            List<Edge> edgeList = new ArrayList<>();
+            nodeList.forEach(node -> edgeList.add(new Edge(node1, node)));
+            edgeList.forEach(edge -> mainGraph.addEdge(edge));
+        });
+        Set<String> nodeNameSet = mainGraph.getEdgeList().stream().
+            flatMap(edge -> Stream.of(edge.getNode1().getName(),edge.getNode2().getName()))
+            .collect(Collectors.toSet());
+        mainGraph.setVerticeCount(nodeNameSet.size());
+        return mainGraph;
+    }
+    public static List<Edge> kargerMinCut(Graph graph)
+    {
+        while (graph.getVerticeCount() > 2){
+            int randomInt = (random.nextInt(0,3500)) % graph.getEdgeList().size();
+            Edge randomEdge = graph.getEdgeList().get(randomInt);
+            graph.cutEdge(randomEdge);
+        }
+        System.out.print(graph.getEdgeList().size()+ " \n");
+        if (graph.getEdgeList().size()==3) {
+            return graph.getEdgeList();
+        }
+        return null;
+    }
+    
+}
