aa98b0e13e3d492ff6af31bca925b00d34cc1374
koytipita <mrtdmrtbn@gmail.com>
2024-12-21 21:45:15 +0300

    day4 - part 2 solution

--- aoc24/day4/Day4.java
+++ aoc24/day4/Day4.java
@@ -1,11 +1,9 @@
 package aoc24.day4;
 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Map;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,12 +12,14 @@ public class Day4 {
     private static Logger logger = Logger.getLogger(Day4.class.getName());
     private static final String EXAMPLE1_FILE_PATH = "aoc24/day4/example1.txt";
     private static final String EXAMPLE2_FILE_PATH = "aoc24/day4/example2.txt";
+    private static final String EXAMPLE3_FILE_PATH = "aoc24/day4/example3.txt";
     private static final String ACTUAL_FILE_PATH =   "aoc24/day4/input.txt";
 
     public static void main(String[] args) {
         Day4 day4 = new Day4();
         List<String> input = utils.FileParseUtil.readLinesFromFile(ACTUAL_FILE_PATH, logger);
         logger.info(() -> String.valueOf(day4.countXMAS(input)));
+        logger.info(() -> String.valueOf(day4.countXMAS2(input)));
     }
 
     private long countXMAS(List<String> input){
@@ -31,9 +31,30 @@ public class Day4 {
         return count;
     }
 
+    private long countXMAS2(List<String> input){
+        return IntStream.range(0, input.getFirst().length() - 2).mapToLong(x ->
+                IntStream.range(0, input.size() - 2)
+                        .filter(y -> input.get(y+1).charAt(x+1) == 'A')
+                        .filter(y ->{
+                            Map<Character, Integer> charMap = new HashMap<>();
+                            Character char1 = input.get(y).charAt(x);
+                            Character char2 = input.get(y).charAt(x+2);
+                            Character char3 = input.get(y+2).charAt(x);
+                            Character char4 = input.get(y+2).charAt(x+2);
+                            charMap.put(char1, charMap.getOrDefault(char1, 0) + 1);
+                            charMap.put(char2, charMap.getOrDefault(char2, 0) + 1);
+                            charMap.put(char3, charMap.getOrDefault(char3, 0) + 1);
+                            charMap.put(char4, charMap.getOrDefault(char4, 0) + 1);
+                            return charMap.getOrDefault('M',0).equals(2) &&
+                                    charMap.getOrDefault('S',0).equals(2) &&
+                                    !char1.equals(char4);
+                        }).count()
+        ).sum();
+    }
+
     private long countXMASDiagonallyUpperLeftToLowerRight(List<String> input){
         return IntStream.range(0, input.getFirst().length() - 3).mapToLong(x ->
-            IntStream.range(0, input.size() -3).filter(y -> {
+            IntStream.range(0, input.size() - 3).filter(y -> {
                 String word = String.valueOf(input.get(y).charAt(x)) +
                         input.get(y + 1).charAt(x + 1) +
                         input.get(y + 2).charAt(x + 2) +
